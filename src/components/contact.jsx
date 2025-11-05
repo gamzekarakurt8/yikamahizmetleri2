0a08b7a3158df9dba81e4bee6ab2af843b2ee020
gamzekarakurt8 <gmzekrkrt@gmail.com>
2025-11-05 15:18:23 +0300

    first commit

---
+++ src/components/contact.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import React from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+    
+    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
+    
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearState();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Hizmet Bölgelerimiz</h2>
+                <div className="service-areas">
+                <span><i className="fa fa-map-marker"></i> Tuzla</span>
+                <span><i className="fa fa-map-marker"></i> Gebze</span>
+                <span><i className="fa fa-map-marker"></i> Pendik</span>
+                <span><i className="fa fa-map-marker"></i> Kartal</span>
+                <span><i className="fa fa-map-marker"></i> Maltepe</span>
+                <span><i className="fa fa-map-marker"></i> Bostancı</span>
+                <span><i className="fa fa-map-marker"></i> Kadıköy</span>
+                <span><i className="fa fa-map-marker"></i> Ümraniye</span>
+              </div>
+              </div>
+                          </div>
+          </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>İletişim Bilgileri</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Adres
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Telefon
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.instagram : "/"}>
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>© 2025 Tüm hakları saklıdır.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
